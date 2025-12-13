68034322e4e019105a593c965e354ccb5299295a
Enzo Basuil <146545169+SuillyCoder@users.noreply.github.com>
2025-12-13 09:55:03 +0800

    Set up TKinter for GUI (to be tampered with later)

--- Visualization.py
+++ Visualization.py
@@ -1,1 +1,11 @@
-print("This is for me!")
+#If you wanna run this, first enter this command in your terminal (not in VS Code):
+# brew install python-tk@3.12
+# or brew install python-tk@3.13
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Hello Tkinter")
+tk.Label(root, text="Welcome to Tkinter!").pack()
+tk.Button(root, text="Click me", command=root.quit).pack()
+root.mainloop()
